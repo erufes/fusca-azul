2717c563ece6087024465480fa9d11ab4ed94631
Yágo Amorim <yagw.dev@gmail.com>
2026-04-29 12:52:50 -0300

    correção de possiveis erros e adicao de comentario

--- esp32/robo.ino
+++ esp32/robo.ino
@@ -1,22 +1,27 @@
-#define TRIG // a definir entrada 
-#define ECHO // a definir entrada 
+#define TRIG // a definir entrada
+#define ECHO // a definir entrada
 
-#define IN1 // a definir entrada 
-#define IN2 // a definir entrada 
-#define IN3 // a definir entrada 
-#define IN4 // a definir entrada 
+#define IN1 // a definir entrada
+#define IN2 // a definir entrada
+#define IN3 // a definir entrada
+#define IN4 // a definir entrada
 
 long distancia;
 
-medirDistancia(){
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG, HIGH);
+long medirDistancia(){
+  digitalWrite(TRIG, LOW); // comeca desligado
+  delayMicroseconds(2); 
+
+  digitalWrite(TRIG, HIGH); // envia o pulso de 10 ms (faz disparar um som ultrassonico)
   delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
+
+  digitalWrite(TRIG, LOW); // termina o pulso
   
-  long duracao = pulseIn(ECHO, HIGH);
-  return duracao * 0.034 / 2;
+  long duracao = pulseIn(ECHO, HIGH, 30000); // pulsein calcula quanto tempo o pino ECHO ficou em HIGH, ele espera no máximo 30ms
+  
+  if (duracao == 0) return 999; // evita erro se nao tiver leitura
+
+  return duracao * 0.034 / 2; // 0.034 = velocidade do som em cm por microsegundo divide por 2 porque o som vai e volta
 }
 
 void frente(){
@@ -24,26 +29,28 @@ void frente(){
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  } 
+} 
   
 void parar(){
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
-  }
+}
+
 void esquerda(){
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  }
+}
+
 void direita(){
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  } 
+} 
 
 void setup(){
   pinMode(TRIG, OUTPUT);
@@ -61,14 +68,15 @@ void loop(){
 
   distancia = medirDistancia();
   
-  if(distancia < 20){
+  Serial.println(distancia); // printa a distancia
+
+  if(distancia < 20 && distancia != 999){
     parar();
     delay(300); // aq é em ms
     esquerda();
     delay(400); // aq é em ms
-  }
+  } 
   else{
     frente();
   }
-  }
-
+}
